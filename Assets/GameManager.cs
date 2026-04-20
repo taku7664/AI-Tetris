1f6579d07aee4261445a9924cf38991f25441846
PPAK_JU <153336193+taku7664@users.noreply.github.com>
2026-04-20 17:48:25 +0900

    add unity project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    public GameObject FieldBox;
+    public GameObject PreviewBox;
+    public TMP_Text   ScoreText;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
