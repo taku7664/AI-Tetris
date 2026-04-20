186844faf2efa4da2b07e243043f042555c05828
PPAK_JU <153336193+taku7664@users.noreply.github.com>
2026-04-20 20:43:28 +0900

    feat: bootstrap Codex scene and game manager

---
+++ Assets/Codex/Codex_GameManager.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace Codex
+{
+    public sealed class Codex_GameManager : MonoBehaviour
+    {
+        public GameObject FieldBox;
+        public GameObject PreviewBox;
+        public TMP_Text ScoreText;
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
